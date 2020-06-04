5aefd3999176d93a18cbfa06576eb939b158cb6a
Srthkdb <srthkdb@iitk.ac.in>
2020-06-04 17:27:52 +0530

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNavigation() {
+  document.getElementById("menu").style.transform = "none" ;
+}
+
+function hideNavigation() {
+  document.getElementById("menu").style.transform = "translate(-110%, 0)" ;
+}
+
+
